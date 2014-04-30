13acc45a1cf82f4b4a3bc801bbf242f34c7214b0
WrinklyNinja <wrinklyninja1@gmail.com>
2014-04-30 23:02:01 +0100

    Added repo getContributors function.

--- octokit.js
+++ octokit.js
@@ -1212,6 +1212,12 @@
             this.getReleases = function() {
               return _request('GET', "" + this.repoPath + "/releases", null);
             };
+            this.getContributors = function(anon) {
+              var data = {
+                anon: anon
+              };
+              return _request('GET', "" + this.repoPath + "/contributors", data);
+            };
           }
 
           return Repository;
